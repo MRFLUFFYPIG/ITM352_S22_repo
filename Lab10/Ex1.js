61f4517ddf97f98f00b989352026d6e0919d6155
Nicholas Sebastian <nfs37@hawaii.edu>
2022-03-09 18:07:20 -1000

    Lab10

---
+++ Lab10/Ex1.js
@@ -0,0 +1,14 @@
+day = 15;
+month = "March";
+year = 1998;
+
+step1 = year - 1900;
+step2 = parseInt(step1/4);
+step3 = step2 + step1;
+step4 = 1;
+step6 = step4 + step3;
+step7 = day + step6;
+step8 = step7;
+step9 = step8%7;
+
+console.log(step9);
