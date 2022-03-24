96cc9291732f4f8af0342c8cd232efcb57121060
Nicholas Sebastian <nfs37@hawaii.edu>
2022-03-23 22:08:14 -1000

    Assignment 1 update

---
+++ Sebastian_Nicholas_Assignment1/products_data.js
@@ -0,0 +1,61 @@
+// Array for products 
+// The products being used will be Shoes 
+
+var products = [
+
+// Nike
+// Source: https://www.nike.com/t/air-max-270-mens-shoes-KkLcGR
+{
+    brand :"Nike",
+    name :"Air Max 270",
+    price : 160.00,
+    image :""
+}
+,
+// Adidas
+// Source: https://www.adidas.com/us/duramo-10-shoes/GW8349.html
+{
+    brand :"Adidas",
+    name :"DURAMO 10 Shoes",
+    price : 65.00,
+    image :""
+}
+,
+// NEW BALANCE
+// Source: https://www.rackroomshoes.com/p/play-gruv-slip-on-sneaker/830361
+{
+    brand :"NEW BALANCE",
+    name :"GRUV SLIP ON SNEAKER - NAVY",
+    price : 49.99,
+    image :""
+}
+,
+// Vans
+// Source: https://www.vans.com/en-us/shoes-c00081/ombre-sunrise-sk8-low-reissue-sf-pvn0a4uwib81
+{
+    brand :"Vans",
+    name :"OMBRE SUNRISE SK8-LOW REISSUE SF",
+    price :75.00,
+    image :""
+}
+,
+// Puma
+// Source: https://us.puma.com/us/en/pd/clyde-core-foil-mens-sneakers/364669.html?dwvar_364669_color=04
+{
+    brand :"Puma",
+    name :"Clyde Core Foil Men's Sneakers",
+    price :70.00,
+    image :""
+}
+,
+// YEEZYs
+// Source: https://stockx.com/adidas-yeezy-knit-rnr-boot-sulfur
+{
+    brand :"YEEZYs",
+    name :"adidas Yeezy Knit RNR Boot",
+    price :599.00,
+    image :""
+}
+]
+
+var items = 6;
