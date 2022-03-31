2f14db14fed8ddb7e38c6c347bc9e4aa301bbb90
Nicholas Sebastian <nfs37@hawaii.edu>
2022-03-31 13:05:43 -1000

    up

--- Lab13/Ex1b.js
+++ Lab13/Ex1b.js
@@ -1,3 +1,12 @@
 var users_reg_data = require("./user_data.json");
 
-console.log(users_reg_data ["kazman"]["password"]);
+// Ex1a
+var filename = "./user_data.json";
+const fs = require("fs");
+var data = fs.readFileSync(filename, 'utf-8');
+var users =JSON.parse(data);
+console.log(users["the kazman"].password);
+
+
+// Ex1b
+// console.log(users_reg_data ["kazman"]["password"]);
